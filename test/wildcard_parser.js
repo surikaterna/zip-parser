73a9bb3c010f2bca5bbfdfab93e454c4f88db3b4
Emil Forsmark <emil.forsmark@surikat.se>
2016-03-24 12:21:15 +0100

    added typechecks for wildcard parser tests

--- test/wildcard_parser.js
+++ test/wildcard_parser.js
@@ -10,26 +10,31 @@ describe('wildcardParser', function describeWildcardParser() {
     it('should return wildcard entity if start with ?', function test() {
       var entity =  wildcardParser('?startwithwildcard');
       entity.codes.length.should.equal(1);
+      entity.type.should.equal('wildcard');
     });
 
     it('should return wildcard entity if wildcard has ? in middle', function test() {
         var entity = wildcardParser('wild?card');
         entity.codes.length.should.equal(1);
+        entity.type.should.equal('wildcard');
     });
 
     it('should return wildcard entity if wildcard has ?? in middle', function test() {
         var entity = wildcardParser('wild??card');
         entity.codes.length.should.equal(1);
+        entity.type.should.equal('wildcard');
     });
 
     it('should return wildcard entity if end with ?', function test() {
       var entity =  wildcardParser('endwithwildcard?');
       entity.codes.length.should.equal(1);
+      entity.type.should.equal('wildcard');
     });
     
     it('should return wildcard entity if end with *', function test() {
       var entity =  wildcardParser('endwithwildcard?');
       entity.codes.length.should.equal(1);
+      entity.type.should.equal('wildcard');
     });
 
   });
